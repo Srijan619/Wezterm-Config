339097492c3a6f4bacea9f112079f73ce13e9c1f
Srijan Personal <chapssrijan@gmail.com>
2025-01-22 07:05:57 +0200

    tab at bottom

--- config/appearance.lua
+++ config/appearance.lua
@@ -34,13 +34,13 @@ return {
    tab_max_width = 25,
    show_tab_index_in_tab_bar = false,
    switch_to_last_active_tab_when_closing_tab = true,
-
+   tab_bar_at_bottom = true,
    -- window
    window_padding = {
       left = 0,
       right = 0,
-      top = 10,
-      bottom = 7.5,
+      top = 0,
+      bottom = 0,
    },
    adjust_window_size_when_changing_font_size = false,
    window_close_confirmation = 'NeverPrompt',
